25feb81f61ac671e54783aad07d53267c5c1cfe3
zxl0715 <zxl0715@163.com>
2019-06-19 12:00:29 +0800

    Update HouseHoldProvider.java
    冰箱

--- household_provider/src/main/java/com/microservice/provider/HouseHoldProvider.java
+++ household_provider/src/main/java/com/microservice/provider/HouseHoldProvider.java
@@ -40,7 +40,7 @@ public class HouseHoldProvider {
 
         List<HouseHold> list = new ArrayList<HouseHold>();
 
-        if ("bingxiang".equals(name)){
+        if ("冰箱".equals(name)){
             list.add(houseHold1);
             return new Result(list);
         }else if ("空调".equals(name)){
